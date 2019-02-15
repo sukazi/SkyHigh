8b7b941da4df95a8fad0eea619a06deea91cabdc
Suha Kazi <sukazi.ucsd.edu>
2019-02-14 22:21:41 -0800

    login fixed

--- routes/tennislink.js
+++ routes/tennislink.js
@@ -1,6 +1,29 @@
-exports.view = function(req,res){
-	res.render('TennisLessons.handlebars');
-}
 exports.view = function(req, res){
   res.render("TennisLessons", {"name": "Gillian Grennan"});
-}
+};
+
+exports.view = function(req, res){
+	res.render("TennisLessons", {
+		"container": [
+		{"userName": "user1",
+		 "image": "profile.pic",
+		 "distance": ".3 miles away",
+		 "bio": "Major: Compuer Engineering",
+		 "id": "container1"
+		},
+		{"userName": "user2",
+		 "image": "profile.pic",
+		 "distance": ".5 miles away",
+		 "bio": "2nd year Human Bio Major",
+		 "id": "container2"
+		},
+		{"userName": "user3",
+		 "image": "profile.pic",
+		 "distance": "1 mile away",
+		 "bio": "College: Revelle. Major: Psychology",
+		 "id": "container3"
+		},
+
+		]
+	});
+};
