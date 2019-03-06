9729d7fb1fd177e6b4c63d9dc6d8c9b7be95a686
Cole Richmond <33361159+colerichmond@users.noreply.github.com>
2019-03-05 18:05:29 -0800

    view/viewAlt boolean

--- routes/homepage.js
+++ routes/homepage.js
@@ -6,5 +6,11 @@
 //JSON
 exports.view = function(req, res){
   res.render("HomePage", {"name": "Gillian Grennan"});
+  projects['viewAlt'] = false
+};
+
+exports.viewAlt = function(req, res){
+  res.render("HomePage", {"name": "Gillian Grennan"});
+  projects['viewAlt'] = true
 };
 
