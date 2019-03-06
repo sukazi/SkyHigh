b92031ac7aa9a6a53e3a111f3079a06723289662
Cole Richmond <33361159+colerichmond@users.noreply.github.com>
2019-03-06 09:21:56 -0800

    remove viewAlt

--- app.js
+++ app.js
@@ -50,7 +50,7 @@ if ('development' == app.get('env')) {
 }
 
 app.get('/', index.view);
-app.get('/viewAlt', index.viewAlt)
+//app.get('/viewAlt', index.viewAlt)
 // Example route
 // app.get('/users', user.list);
 
